52184295510eacbb0e833c30fd3b054bfd62cbdb
eric-chambonneau <eric.chambonneau@gmail.com>
2022-10-20 22:10:05 +0200

    1init

--- estimation.php
+++ estimation.php
@@ -4,11 +4,11 @@
 echo "Estimation en cours...";
 
 // récupérer une donnée du formulaire
- echo $_POST['annee'];
+//  echo $_POST['annee'];
 
 // variable pour récupérer l'année
-// $nom = $_POST['nom'];
-// $type = $_POST['type']
-// $annee = $_POST['annee'];
+$nom = $_POST['nom'];
+$type = $_POST['type']
+$annee = $_POST['annee'];
 
-// echo "l oeuvre s'appelle :" . $nom;
+echo "l oeuvre s'appelle :" . $nom;
